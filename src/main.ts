0dc032704715c660befcee2f30636253e6e4d6d6
77-ahmad-collab <ahmaq098@gmail.com>
2022-01-07 02:50:00 +0500

    server test case ready

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{cors:true});
   console.log('app is listening on port 3000');
   await app.listen(3000);
 }
